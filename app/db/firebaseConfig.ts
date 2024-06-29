754eb820ae9f44534d541d0a25204f4394976c77
Matheus Dantas <matheus.kohaku.dantas@gmail.com>
2024-06-29 03:43:09 -0300

    fix security issue

--- app/db/firebaseConfig.ts
+++ app/db/firebaseConfig.ts
@@ -9,14 +9,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDA_GOG6cgLpjOAR9W9cglf5HChulSHMek",
-  authDomain: "cloudent-81740.firebaseapp.com",
-  projectId: "cloudent-81740",
-  storageBucket: "cloudent-81740.appspot.com",
-  messagingSenderId: "534171088458",
-  appId: "1:534171088458:web:dcf8a214bacd416a0098b3",
-  measurementId: "G-DLBYTKGXH6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
